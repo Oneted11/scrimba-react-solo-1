91685ef43c00dc0ac385c40df53d25b600d769d1
Oneted11 <muriithited@gmail.com>
2022-06-14 10:24:32 +0300

    feat:major updates, close

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
+# Learnings
+
+## Add fontawesome to react
+
+Downloading fontawesome.min.css and linking it in index.html might not work so this is what does as described on [stackoverflow](https://stackoverflow.com/questions/42522233/react-fontawesome-not-displaying-icons)
+
+```html
+<link
+  href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-
+awesome.min.css"
+  rel="stylesheet"
+  integrity="sha384-
+wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+  crossorigin="anonymous"
+/>
+```
+
+<br><br>
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import "./App.css";
 import profilepic from "./profile.jpg";
+
 function App() {
   return (
     <div className="container">
-      <img src={profilepic} alt="ted"></img>
+      <div className="card">
+        <img src={profilepic} alt="ted"></img>
+        <h2>Ted Mureithi</h2>
+        <p className="describe">Frontend Developer</p>
+        <p className="site">tedmureithi.website</p>
+        <section className="buttons">
+          <button>
+            <i className="fa fa-envelope" />
+            Email
+          </button>
+          <button className="linkedin">
+            <i className="fa fa-linkedin-square"></i> Linkedin
+          </button>
+        </section>
+        <div className="text-area">
+          <h3>About</h3>
+          <p>
+            I am a frontend developer with a particular interest in making
+            things simple and automating daily tasks. I try to keep up with
+            security and best practices, and am always looking for new things to
+            learn.
+          </p>
+          <h3>Interests</h3>
+          <p>
+            Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+            Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+          </p>
+        </div>
+        <div className="bottom-bar">
+          <i className="fa fa-twitter-square"></i>
+          <i className=""></i>
+          <i className=""></i>
+          <i className=""></i>
+        </div>
+      </div>
     </div>
   );
 }
